d7b2feba3fea24ea86695ad15a332085608bc578
Brandon <brandonskerritt51@gmail.com>
2018-02-03 15:52:25 +0000

    This is an automated script to quickly update this Git repo. No important features here.

--- scrollReveal.js
+++ scrollReveal.js
@@ -2,3 +2,5 @@ window.sr = ScrollReveal({ reset: true });
 sr.reveal('.containerone');
 sr.reveal('.containertwo');
 sr.reveal('.containerthree');
+sr.reveal('.containerFour');
+sr.reveal('.containerFive');
